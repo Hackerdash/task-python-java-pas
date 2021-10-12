a0f13eeef91455611b341e27269c8d8b53a70e34
Dinesh Naik <32676675+Hackerdash@users.noreply.github.com>
2021-10-12 16:15:25 +0530

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,4 +1,4 @@
 import os
 os.system('wget "https://www.designite-tools.com/static/download/DJE/DesigniteJava.jar"')
-os.system('java -jar DesigniteJava.jar -ci -repo $GITHUB_REPOSITORY -pat ${{ secrets.PAT }} ')
-os.system('curl -X PUT -H "Authorization: Token ${{ secrets.QSCORED_API_KEY }}" -H "repository-link:https://github.com/" + GITHUB_REPOSITORY -H "username: " -H "Content-Type: mulitpart/form-data" --url "https://qscored.com/api/upload/file.xml?is_open_access=off&version=$GITHUB_RUN_NUMBER&project_name=" -F "file=@Designite_output/DesigniteAnalysis.xml" ')
+def javacomplie(PAT):
+    os.system('java -jar DesigniteJava.jar -ci -repo $GITHUB_REPOSITORY -pat  '+PAT)
