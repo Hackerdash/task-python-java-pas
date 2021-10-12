8ec337e9adc0abb526c1b627e9cbbf6bf011d40f
Dinesh Naik <32676675+Hackerdash@users.noreply.github.com>
2021-10-12 16:28:25 +0530

    Update pi.py

--- pi.py
+++ pi.py
@@ -1,4 +1,6 @@
 import os
+import sys
 os.system('wget "https://www.designite-tools.com/static/download/DJE/DesigniteJava.jar"')
 def javacomplie(PAT):
     os.system('java -jar DesigniteJava.jar -ci -repo $GITHUB_REPOSITORY -pat  '+PAT)
+javacomplie(sys.argv[1])
